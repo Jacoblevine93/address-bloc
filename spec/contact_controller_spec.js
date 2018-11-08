86aff700aee5fa63d83cee2d9ae34965d76cd37d
Jacob Levine <Jacob.levine93@protonmail.com>
2018-11-08 14:37:05 -0700

    Checkpoint 4 In Progress

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -14,9 +14,33 @@ describe("Contact Controller", () => {
 		});
 	});
 
-	//it('should be defined', () => {
-	//	expect(ContactController).toBeDefined();
-	//});
+	describe('#getContacts()', () => {
+		it ('should return an empty array when no contacts are available', (done) => {
+			this.book.getContacts()
+			.then((contacts) => {
+				expect(contacts.length).toBe(0)l
+			})
+			.catch((err) => {
+				console.log(err);
+				done();
+			});
+		});
+
+		it('should return an array of contacts when contacts are available', (done) => {
+			this.book.addContact('Alice', '001-101-1010', "test@aol.com")
+			.then(() => {
+				this.book.getContacts()
+				.then((contacts) => {
+					expect(contacts.length).toBe(1);	
+					done();
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+				done();
+			});			
+		});
+	});
 
 	describe('#addContact()', () => {
 		it('should add a single contact into the book', (done) => {
